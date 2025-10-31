23daefa505ed97afb85750241b7e4338ef005b7b
Danny <chasebreton.cbreton@gmail.com>
2025-10-31 20:48:21 +0000

    ..

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -38,7 +38,7 @@ const Header = () => {
           {/* Logo */}
           <Link to="/" className="flex items-center space-x-3">
             <img 
-              src="/xample-logo.png" 
+              src="https://files.fm/u/9mabmss46p" 
               alt="X-Ample Development Studios" 
               className="h-10 w-auto"
             />
