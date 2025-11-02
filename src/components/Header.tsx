c7eca8d7b15198cc6064ddc1030f8fa9e2831a24
Danny <info@x-ampledevelopment.com>
2025-11-02 18:26:29 +0000

    .

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -38,11 +38,6 @@ const Header = () => {
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
           <Link to="/" className="flex items-center space-x-3">
-            <img 
-              src="https://i.imgur.com/4bSGPHi.png" 
-              alt="X-Ample Development Studios" 
-              className="h-10 w-auto"
-            />
             <span className="text-xl font-bold text-white-900">X-Ample Studios</span>
           </Link>
 
