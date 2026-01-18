6ec9ae1be4c5ac531d7f81f0acc73cd31fd341c8
Danny <info@x-ampledevelopment.com>
2026-01-18 15:01:09 +0000

    Add NotFound page import to App component

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
 import Team from "./pages/Team";
 import Store from "./pages/Store";
+import NotFound from "./pages/NotFound";
 
 
 const queryClient = new QueryClient();
