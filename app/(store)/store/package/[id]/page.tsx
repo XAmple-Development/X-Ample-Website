1700db723a244cc018e505db1e249514691f0206
Danny <info@x-ampledevelopment.com>
2026-01-19 21:05:10 +0000

    ..

--- app/(store)/store/package/[id]/page.tsx
+++ app/(store)/store/package/[id]/page.tsx
@@ -1,15 +1,9 @@
 import PackageClient from "./package-client";
 
-export default function PackagePage({
-  params,
-}: {
-  params: { id?: string; packageId?: string; slug?: string };
-}) {
-  const id = params.id ?? params.packageId ?? params.slug;
-
+export default function PackagePage({ params }: { params: { id: string } }) {
   return (
     <main className="mx-auto max-w-4xl px-4 py-10">
-      <PackageClient id={id} />
+      <PackageClient id={params.id} />
     </main>
   );
 }
